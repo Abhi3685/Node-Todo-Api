ebbf99d134aa65b2ff4811177a3d9856fd2c1903
Abhi3685 <35190737+Abhi3685@users.noreply.github.com>
2019-01-04 11:10:03 +0530

    Add Queries Playground File

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -7,22 +7,22 @@ var { User } = require('./../app/models/user');
 var id = '5c2e401579bc3121885d29d8';
 var userId = '5c2cc81b2cc5ea2e983cff97';
 
-// if(!ObjectID.isValid(id)){
-// 	console.log('Id is invalid');
-// } else {
-// 	Todo.findById(id, (err, todo) => {
-// 		if(!todo) return console.log('Id not found');
-// 		console.log('Todo By Id', todo);
-// 	});
-// }
+if(!ObjectID.isValid(id)){
+	console.log('Id is invalid');
+} else {
+	Todo.findById(id, (err, todo) => {
+		if(!todo) return console.log('Id not found');
+		console.log('Todo By Id', todo);
+	});
+}
 
-// Todo.find({ _id: id }, (err, todos) => {
-// 	console.log('Todos', todos);
-// });
+Todo.find({ _id: id }, (err, todos) => {
+	console.log('Todos', todos);
+});
 
-// Todo.findOne({ _id: id }, (err, todo) => {
-// 	console.log('Todo', todo);
-// });
+Todo.findOne({ _id: id }, (err, todo) => {
+	console.log('Todo', todo);
+});
 
 User.findById(userId, (err, user) => {
 	if(err){
